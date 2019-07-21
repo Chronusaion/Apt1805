758237077532a06ae0b0221fee6b775465d36986
Xavier Santos <46350495+Chronusaion@users.noreply.github.com>
2019-07-21 00:48:24 -0400

    slow move Camera
    slow move Camera

--- Game CodeName(Twin-Stick-Dungeon)/Assets/Scripts/PlayerMovement.cs
+++ Game CodeName(Twin-Stick-Dungeon)/Assets/Scripts/PlayerMovement.cs
@@ -5,30 +5,37 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
-    
+    public float moveRotacion;
     private Rigidbody2D myRb;
     
     private Vector2 MoveVelocity;
 
-    public float offSet;
+    public float offSetRotacion;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        myRb = GetComponent<Rigidbody2D>(); 
+        myRb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         MoveVelocity = moveInput.normalized * moveSpeed;
 
 
-        Vector3 diferencial = (Input.mousePosition) + transform.position;
+        /*Vector3 diferencial = Camera.main.ScreenToViewportPoint(Input.mousePosition) - transform.position;
         float rotacionZ = Mathf.Atan2(diferencial.y, diferencial.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rotacionZ + offSet);
+        transform.rotation = Quaternion.Euler(0f, 0f, rotacionZ + offSetRotacion);*/
+
+
+
     }
 
     private void FixedUpdate()
---
+++ Game CodeName(Twin-Stick-Dungeon)/Assets/Scripts/Rotacion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour
+{
+    public float offSetRotacion;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diferencial = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotacionZ = Mathf.Atan2(diferencial.y, diferencial.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotacionZ + offSetRotacion);
+    }
+}
